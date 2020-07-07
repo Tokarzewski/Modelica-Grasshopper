4299a7147ce5257c4d818b5eea8a5a224531a4f4
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2020-07-07 19:23:54 +0200

    Update SimulateClass.py

--- Python/SimulateClass.py
+++ Python/SimulateClass.py
@@ -1,14 +1,13 @@
-from OMPython import OMCSessionZMQ
-omc = OMCSessionZMQ()
-
-#ModelPath=omc.sendExpression("getInstallationDirectoryPath()") + "/share/doc/omc/testmodels/" + "BouncingBall.mo"
 from OMPython import ModelicaSystem
-mod=ModelicaSystem(ModelPath,ClassName)
-#3 arguments #mod=ModelicaSystem(ModelPath, ClassName, ["Modelica"])
-#4 arguments #mod=ModelicaSystem(ModelPath, ClassName, commandLineOptions="-d=newInst")
+
+#Input Model
+mod=ModelicaSystem(ModelPath,ClassName) #2 arguments
+#mod=ModelicaSystem(ModelPath, ClassName, ["Modelica"]) #3 arguments 
+#mod=ModelicaSystem(ModelPath, ClassName, commandLineOptions="-d=newInst") #4 arguments
+
+#Outputs
 Quantities = mod.getQuantities()
 SimOptions = mod.getSimulationOptions()
-
 if RunSimulation == True:
     mod.simulate()
     Solutions = list(mod.getSolutions())
