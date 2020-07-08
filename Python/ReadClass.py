08f628c1e7f80a1040377a607552930dc7fe0409
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2020-07-08 13:08:46 +0200

    Update ReadClass.py

--- Python/ReadClass.py
+++ Python/ReadClass.py
@@ -1,6 +1,8 @@
 from OMPython import OMCSessionZMQ  
 
 omc = OMCSessionZMQ()
-omc.sendExpression('loadFile("' + ModelPath + '")')
-#print("loadFile(" + '"' + ModelPath +'")') #Print Expression
-output = omc.sendExpression("instantiateModel("+ClassName+")")
+omc.sendExpression('loadFile("' + ModelPath +'")')
+#print("loadFile(" + ModelPath + '")') #Print Expression
+
+ClassContent = omc.sendExpression("instantiateModel("+ ClassName +")")
+ClassNames = list(omc.sendExpression("getClassNames()"))
