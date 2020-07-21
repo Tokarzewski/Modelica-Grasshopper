8ac0ba710c99fd9009c4c17bbe3dfcc1a3383abf
Tokarzewski <bartlomiej.tokarzewski@gmail.com>
2020-07-22 00:57:21 +0200

    Delete ReadClass.py

--- Python/ReadClass.py
+++
@@ -1,8 +0,0 @@
-from OMPython import OMCSessionZMQ  
-
-omc = OMCSessionZMQ()
-omc.sendExpression('loadFile("' + ModelPath + '")')
-#print("loadFile(" + ModelPath + '")') #Print Expression
-
-ClassContent = omc.sendExpression("instantiateModel("+ ClassName +")")
-ClassNames = list(omc.sendExpression("getClassNames()"))
